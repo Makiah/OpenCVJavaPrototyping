7e012fa7cbdab8f01e920181fe05afd05b1d5ee4
Makiah Bennett <makiahtbennett@gmail.com>
2017-12-07 16:28:35 -0500

    Keep improving crypto detector

--- src/analyzers/CryptoboxPixelLab.java
+++ src/analyzers/CryptoboxPixelLab.java
@@ -27,46 +27,48 @@ public class CryptoboxPixelLab implements FrameAnalyzer
     @Override
     public void analyze(Mat raw)
     {
+        Mat kernel = Mat.ones(2, 2, CvType.CV_32F);
+
+        // Resize picture to phone space.
         Imgproc.resize(raw, raw, new Size(700, 360));
 
-        ///// Get rid of high luminance pixels (working in HLS space) ////
+        Imgproc.blur(raw, raw, new Size(3, 3));
+
+        // Work in Hue Luminance Saturation Space.
         Imgproc.cvtColor(raw, raw, Imgproc.COLOR_BGR2HLS);
+        LinkedList<Mat> channels = new LinkedList<>();
+        Core.split(raw, channels);
 
-        Mat before = raw.clone();
-        Mat after = raw.clone();
+        // Use adaptive threshold to artificially create luminance contours.
+        Imgproc.adaptiveThreshold(channels.get(1), channels.get(1), 150, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
+        //Imgproc.blur(channels.get(1), channels.get(1), new Size(1, 10));
+        Imgproc.erode(channels.get(1),channels.get(1),kernel);
+        Imgproc.dilate(channels.get(1),channels.get(1),kernel);
+        new CVJPanel(channels.get(1), "Luminance Thresholds", 0, 0);
 
-        // Before
-        LinkedList<Mat> channelsBefore = new LinkedList<>();
-        Core.split(before, channelsBefore);
-//        CLAHE claheBefore = Imgproc.createCLAHE(4, new Size(50,200));
-//        claheBefore.apply(channelsBefore.get(1), channelsBefore.get(1));
-        new CVJPanel(channelsBefore.get(1), "Before 1", 0, 0);
-        Imgproc.adaptiveThreshold(channelsBefore.get(1), channelsBefore.get(1), 150, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 2);
-        //Imgproc.adaptiveThreshold();
-        new CVJPanel(channelsBefore.get(1), "Before 1", 0, 0);
+        // Merge artificial contours into raw, and move back to normal color space.
+        Core.merge(channels, raw);
+        Imgproc.cvtColor(raw, raw, Imgproc.COLOR_HLS2BGR);
+        new CVJPanel(raw, "Raw", 500, 0);
 
-        Core.merge(channelsBefore, before);
-        Imgproc.cvtColor(before, before, Imgproc.COLOR_HLS2BGR);
-//        Imgproc.cvtColor(before, before, Imgproc.COLOR_HLS2BGR);
-        new CVJPanel(before, "Before 1", 0, 0);
+        // Extract blue channel
+        LinkedList<Mat> bgrChannels = new LinkedList<>();
+        Core.split(raw, bgrChannels);
+        new CVJPanel(bgrChannels.get(0), "Blue channel", 0, 380);
 
-////        LinkedList<Mat> channelsBefore2 = new LinkedList<>();
-////        Imgproc.cvtColor(before, before, Imgproc.COLOR_BGR2HLS);
-//        Core.split(before, channelsBefore);
-//        channelsBefore.get(1).setTo(new Scalar(100));
-//        channelsBefore.get(2).setTo(new Scalar(255));
-//        Core.merge(channelsBefore, before);
-//        Imgproc.cvtColor(before, before, Imgproc.COLOR_HLS2BGR);
-//        new CVJPanel(before, "Before 2", 500, 0);
-//
-//        //After
-//        LinkedList<Mat> channelsAfter = new LinkedList<>();
-//        Core.split(after, channelsAfter);
-//        channelsAfter.get(1).setTo(new Scalar(100));
-//        channelsAfter.get(2).setTo(new Scalar(255));
-//        Core.merge(channelsAfter, after);
-//        Imgproc.cvtColor(after, after, Imgproc.COLOR_HLS2BGR);
-//        new CVJPanel(after, "After", 0, 380);
+        // Extract blue regions using ANOTHER adaptive threshold.
+        Mat blueRegion = bgrChannels.get(0).clone();
+//        Mat structure = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(10,2));
+//        Imgproc.morphologyEx(blueRegion,blueRegion,Imgproc.MORPH_CLOSE, structure);
+
+        Mat kernelRectangle = Mat.ones(15, 1, CvType.CV_32F);
+        //Mat structure = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(1,15));
+        //Imgproc.morphologyEx(blueRegion,blueRegion,Imgproc.MORPH_CLOSE, structure);
+        Imgproc.dilate(blueRegion, blueRegion, kernel);
+
+        Core.subtract(blueRegion, new Scalar(100), blueRegion);
+
+        new CVJPanel(blueRegion, "Blue channel", 500, 380);
 
 
         if (true)
@@ -80,8 +82,8 @@ public class CryptoboxPixelLab implements FrameAnalyzer
                 new Scalar(255, 120, 52), raw);
 
         ////// Get rid of excessive noise ///////
-        Imgproc.blur(raw, raw, new Size(2, 10));
-        Mat kernel = Mat.ones(15,3, CvType.CV_32F);
+        Imgproc.blur(raw, raw, new Size(1, 5));
+        //Mat kernel = Mat.ones(15,3, CvType.CV_32F);
         Imgproc.erode(raw,raw,kernel);
         Imgproc.dilate(raw,raw,kernel);
         Imgproc.threshold(raw, raw, 200, 255, Imgproc.THRESH_BINARY);
