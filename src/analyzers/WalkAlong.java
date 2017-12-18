0ae42b9e91949460ad711f5930b56955d1c2d168
Makiah Bennett <makiahtbennett@gmail.com>
2017-12-18 13:22:22 -0500

    More adaptive thresholding

--- src/analyzers/WalkAlong.java
+++ src/analyzers/WalkAlong.java
@@ -22,9 +22,60 @@ public class WalkAlong implements FrameAnalyzer
 
     private int estimatedDistCM = 100;
 
-    /**
-     * This is where pretty much of all of the prototyping code should go.
-     */
+    enum CryptoColor
+    {
+        PRIMARY, WHITE, NONE
+    }
+
+    private CryptoColor getProminentColorFrom(CryptoColor[] colors)
+    {
+        int primaries = 0, whites = 0, nones = 0;
+
+        for (CryptoColor color : colors)
+        {
+            switch (color)
+            {
+                case PRIMARY:
+                    primaries++;
+                    break;
+
+                case WHITE:
+                    whites++;
+                    break;
+
+                case NONE:
+                    nones++;
+                    break;
+            }
+        }
+
+        // Return prominent color.
+        if (primaries > whites && primaries > nones)
+            return CryptoColor.PRIMARY;
+        else if (whites > primaries && whites > nones)
+            return CryptoColor.WHITE;
+        else
+            return CryptoColor.NONE;
+    }
+
+    private CryptoColor[] blurColumnArray(CryptoColor[] pixelColumn, int radius)
+    {
+        // Sort of "blur" the barcode type array with a radius of 5.
+        for (int i = 0; i < pixelColumn.length - radius; i += radius)
+        {
+            CryptoColor[] radiusArray = new CryptoColor[radius];
+            for (int j = 0; j < radius; j++)
+                radiusArray[j] = pixelColumn[i + j];
+
+            CryptoColor prominentColor = getProminentColorFrom(radiusArray);
+
+            for (int j = 0; j < radius; j++)
+                pixelColumn[i + j] = prominentColor;
+        }
+
+        return pixelColumn;
+    }
+
     @Override
     public void analyze(Mat raw)
     {
@@ -47,7 +98,7 @@ public class WalkAlong implements FrameAnalyzer
         Imgproc.cvtColor(raw, raw, Imgproc.COLOR_BGR2HSV);
         for (int x = 0; x < raw.cols(); x++)
         {
-            int[] pixelColumn = new int[raw.height()];
+            CryptoColor[] pixelColumn = new CryptoColor[raw.height()];
 
             int bluePixels = 0, whitePixels = 0, otherPixels = 0;
 
@@ -56,56 +107,79 @@ public class WalkAlong implements FrameAnalyzer
             {
                 double[] pixel = raw.get(y, x);
 
-                if (pixel[0] > 30 && pixel[0] > 150)
+                if (pixel[0] > 100 - .1 * (pixel[2] - 90) && pixel[0] < 190 - .1 * (pixel[2] - 90) && pixel[1] > 50 - .2 * (pixel[2] - 90))
                 {
-                    pixelColumn[y] = 1;
+                    pixelColumn[y] = CryptoColor.PRIMARY;
                     bluePixels++;
-                    raw.put(y, x, 255, 0, 0); // blue
                 }
-                else if (pixel[1] < 50)
+                else if (pixel[1] < 56 && pixel[2] > 69)
                 {
-                    pixelColumn[y] = 2;
+                    pixelColumn[y] = CryptoColor.WHITE;
                     whitePixels++;
-                    raw.put(y, x, 255, 255, 255); // white
                 }
                 else
                 {
-                    pixelColumn[y] = 0;
-                    raw.put(y, x, 0, 0, 0);
+                    pixelColumn[y] = CryptoColor.NONE;
                 }
             }
 
             // Short circuit to the next loop if we detect very few blue pixels.
-            if (bluePixels < camResolution.height / 5)
-                continue;
+//            if (bluePixels < camResolution.height / 5 && whitePixels < camResolution.height / 10)
+//                continue;
 
-            // Sort of "blur" the barcode type array with a radius of 5.
-            int radius = 5;
-            for (int i = 0; i < pixelColumn.length; i += radius)
+            // Blur the column.
+            pixelColumn = blurColumnArray(pixelColumn, 5);
+
+            // Apply this back to the original image and just see what's up with it now.
+            for (int j = 0; j < pixelColumn.length; j++)
             {
-                int[] mostPopular = new int[3];
-
-                for (int j = 0; j < radius; j++)
+                switch(pixelColumn[j])
                 {
-                    // add one to mostPopular[0, 1, or 2]
-                    mostPopular[pixelColumn[i + j]]++;
-                }
+                    case NONE:
+                        raw.put(j, x, 0, 0, 0);
+                        break;
 
-                int prominentColor = 0;
-                for (int j = 0; j < mostPopular.length; j++) {
-                    prominentColor = mostPopular[j] > mostPopular[prominentColor] ? j : prominentColor;
-                }
+                    case PRIMARY:
+                        raw.put(j, x, 70, 255, 120);
+                        break;
 
-                for (int j = 0; j < radius; j++)
-                {
-                    pixelColumn[i + j] = prominentColor;
+                    case WHITE:
+                        raw.put(j, x, 0, 0, 255);
+                        break;
                 }
             }
 
-            // Now figure out the distance from the column using this information.
+            // Now start filtering out non-columns
+            int[] pixelQuantities = new int[3];
+            for (int j = 0; j < pixelColumn.length; j++)
+            {
+                switch (pixelColumn[j])
+                {
+                    case PRIMARY:
+                        pixelQuantities[0]++;
+                        break;
 
+                    case WHITE:
+                        pixelQuantities[1]++;
+                        break;
+
+                    case NONE:
+                        pixelQuantities[2]++;
+                        break;
+                }
+            }
+
+            // If this isn't a column, then make it appear as empty.
+//            if (!(pixelQuantities[0] > .1 * camResolution.height && pixelQuantities[1] > .1 * camResolution.height))
+//            {
+//                for (int j = 0; j < pixelColumn.length; j++)
+//                {
+//                    raw.put(j, x, 0, 0, 0);
+//                }
+//            }
         }
+
         Imgproc.cvtColor(raw, raw, Imgproc.COLOR_HSV2BGR);
-        new CVJPanel(raw, "Blue/White", 550, 0);
+        new CVJPanel(raw, "Processed", 550, 0);
     }
 }
